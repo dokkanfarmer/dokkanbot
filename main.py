628e5469e20c4ced1d28af1f4cead4f2f27c9ffd
nars3112 <127084225+nars3112@users.noreply.github.com>
2023-09-30 23:29:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-# -*- coding: utf-8 -*-
+import snowmate_collector
+import pathlib
+snowmate_collector.start(
+    project_path=str(pathlib.Path("/home/oni/dokkan bot repo")),  
+    project_id="651594d37fd78c9fb3eac42a",
+    client_id="19bfe8b-e8c9-4ac3-8868-82c94eb502fc",
+    secret_key="77e82f20-26b1-413a-b3cd-4c327bef5289",
+)
 import requests
 import base64
 
@@ -12,9 +19,9 @@ def login(license_key):
 if __name__ == "__main__":
 	license=raw_input('your license key: ')
 	if len(license)<>32:
-		print 'invalid license, you can buy one here https://forum.dbzdb.pw/store/category/1-vip-forum-time/'
+		print 'invalid license, you can buy one here placeholder/'
 		exit(1)
 	if login(license):
 		print 'you can now use the bot :)'
 	else:
-		print 'invalid license, you can buy one here https://forum.dbzdb.pw/store/category/1-vip-forum-time/'
+		print 'invalid license, you can buy one here placeholder/'
